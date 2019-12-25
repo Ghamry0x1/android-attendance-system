fc3f198eca95bc0f95be27b11ed9d63383c5e9db
Aly Mohamed Aly <mo7tarif1@hotmail.com>
2019-12-25 18:51:34 +0200

    Added scanner logic

--- Student-app/app/src/main/java/com/example/student_app/MainActivity.java
+++ Student-app/app/src/main/java/com/example/student_app/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.student_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Sign = findViewById(R.id.SignInBtn);
+        Sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, ScanActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
