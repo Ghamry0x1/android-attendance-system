e148a7739d8140bc62ca4f58eaf1fe0ae77e18ef
Mohamed El Ghamry <mohamed.elghamry@outlook.com>
2019-12-25 18:05:22 +0200

    add static files
    Signed-off-by: Mohamed El Ghamry <mohamed.elghamry@outlook.com>

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(express.static('dist'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
